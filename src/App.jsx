fb6e62141c244b59edc01d955e92e12d48596721
praveen <praveenmech483@gmail.com>
2023-11-03 08:23:41 +0530

    confirm

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,86 @@
-import React, { useState } from 'react'
+//implement simple CRUD 
 
-function App() {
 
-  const [count, setcount] = useState(0);
+import React, { useEffect, useState } from 'react'
+
+
+function App(props) {
+//method 1;
+
+  // //define states
+  // let [notes, setnotes] = useState([]);
+  // //get the data
+  // useEffect(() => {
+  //   // run for the first time and only one time
+  //   setnotes(props.notes);
+  // }, []);
+
+  //method 2;
+
+  let [notes, setNotes] = useState(props.notes);
+  console.log(notes);
+  let [showStatus, setShowStatus] = useState('all');
+
+  let handleStatuschange = (event) => {
+    setShowStatus(event.target.value)
+  }
+
+  let filterNotes = (notes, showStatus) => {
+    switch (showStatus) {
+      case 'all':
+        return notes;
+      case 'imp':
+        return notes.filter(note => note.important === true);
+      case 'non imp':
+        return notes.filter(note => note.important === false);
+    }
+  }
+
+  let notesFiltered = filterNotes(notes, showStatus);
   return (
     <div>
-      <p>count: {count}</p>
-      <button onClick={() => setcount(count + 1)}>incremental</button>
-      <button onClick={() => setcount(count - 1)}>decrememtal</button>
-      <button onClick={() => setcount(0)}>Reset</button>
+      <h1>Notes</h1>
+      <label>
+        <input
+          type='radio'
+          name='filter'
+          value='all'
+          checked={showStatus==='all'}
+          onChange={handleStatuschange}
+        />
+         All Notes
+      </label>
+      <label>
+        <input
+          type='radio'
+          name='filter'
+          value='imp'
+          checked={showStatus==='imp'}
+          onChange={handleStatuschange}
+        />
+        Important Notes
+      </label>
+      <label>
+        <input
+          type='radio'
+          name='filter'
+          value='non imp'
+          checked={showStatus==='non imp'}
+          onChange={handleStatuschange}
+        />
+        Non Important Notes
+      </label>
+      <ul>
+        {
+          notesFiltered.map(notes => {
+            return (
+              <li key={notes.id}>{notes.content}</li>
+              
+            )
+          })
+        }
+      </ul>
+
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,27 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const notes = [
+  {
+    id: 1,
+    content: "praveenkumar BE",
+    important: true
+  },
+  {
+    id: 2,
+    content: "working as a IT support Engineer",
+    important: false
+  },
+  {
+    id: 3,
+    content: "currently full stack web developer",
+    important: true
+  },
+  {
+    id: 4,
+    content: "in feature businessman",
+    importand: false
+  },
+];
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
