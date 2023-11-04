8a932ef686b8d943eb6aa0784e6aa19170ad6d01
praveen <praveenmech483@gmail.com>
2023-11-04 11:01:22 +0530

    CRUD created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 //implement simple CRUD 
 
 
+import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 
 
-function App(props) {
-//method 1;
+function App() {
+  //method 1;
 
   // //define states
   // let [notes, setnotes] = useState([]);
@@ -14,12 +15,33 @@ function App(props) {
   //   // run for the first time and only one time
   //   setnotes(props.notes);
   // }, []);
-
   //method 2;
 
-  let [notes, setNotes] = useState(props.notes);
-  console.log(notes);
+  let [notes, setNotes] = useState([]);
   let [showStatus, setShowStatus] = useState('all');
+  let [newNoteContent, setnewNoteContent] = useState('');
+  let [newNoteImportant, setnewNoteImportant] = useState('');
+
+  const fetchNotes = async () => {
+    await axios.get("http://localhost:3000/notes")
+    .then(response => {
+      setNotes(response.data)
+    })
+  }
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/notes")
+      .then(response => {
+        setNotes(response.data);
+      })
+      .catch(error => {
+        console.error("error fetching data", error)
+      });
+  }, [])
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
 
   let handleStatuschange = (event) => {
     setShowStatus(event.target.value)
@@ -37,6 +59,24 @@ function App(props) {
   }
 
   let notesFiltered = filterNotes(notes, showStatus);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    let noteObject = {
+      id: notes.length + 1,
+      content: newNoteContent,
+      important: newNoteImportant === 'true'
+    }
+    setNotes(notes.concat(noteObject))
+    setnewNoteContent('');
+    setnewNoteImportant('');
+
+    console.log('add new note adding...');
+    axios.post('http://localhost:3000/notes', noteObject)
+      .then(response => {
+      console.log('new note added....')
+    })
+  }
   return (
     <div>
       <h1>Notes</h1>
@@ -45,17 +85,17 @@ function App(props) {
           type='radio'
           name='filter'
           value='all'
-          checked={showStatus==='all'}
+          checked={showStatus === 'all'}
           onChange={handleStatuschange}
         />
-         All Notes
+        All Notes
       </label>
       <label>
         <input
           type='radio'
           name='filter'
           value='imp'
-          checked={showStatus==='imp'}
+          checked={showStatus === 'imp'}
           onChange={handleStatuschange}
         />
         Important Notes
@@ -65,22 +105,44 @@ function App(props) {
           type='radio'
           name='filter'
           value='non imp'
-          checked={showStatus==='non imp'}
+          checked={showStatus === 'non imp'}
           onChange={handleStatuschange}
         />
         Non Important Notes
       </label>
       <ul>
         {
-          notesFiltered.map(notes => {
+          notesFiltered.map(note => {
             return (
-              <li key={notes.id}>{notes.content}</li>
-              
+              <li key={note.id}>{note.content}</li>
+
             )
           })
         }
       </ul>
-
+      <hr></hr>
+      <form onSubmit={addNote}>
+        <label>
+          <h1>Add New Note</h1>
+          content: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+          <input
+            type='text'
+            typeof='submit'
+            onChange={(e) => setnewNoteContent(e.target.value)}
+            value={newNoteContent}
+          />
+          <br></br><br></br>
+          important: &nbsp; &nbsp; &nbsp; &nbsp;
+          <select onChange={(e) => setnewNoteImportant(e.target.value)}
+            value={newNoteImportant}>
+            <option>--select--</option>
+            <option>true</option>
+            <option>false</option>
+          </select>
+          <br></br><br></br>
+          <button type='submit'>Add new note</button>
+        </label>
+      </form>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,27 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
-const notes = [
-  {
-    id: 1,
-    content: "praveenkumar BE",
-    important: true
-  },
-  {
-    id: 2,
-    content: "working as a IT support Engineer",
-    important: false
-  },
-  {
-    id: 3,
-    content: "currently full stack web developer",
-    important: true
-  },
-  {
-    id: 4,
-    content: "in feature businessman",
-    importand: false
-  },
-];
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App  />)
